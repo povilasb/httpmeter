21b81624a3f94c60eb2ca2c7eaa1f3cb4e182394
Povilas Balciunas <balciunas90@gmail.com>
2017-01-31 08:28:04 +0200

    Distribute requests evenly among forked worker processes

--- httpmeter/__main__.py
+++ httpmeter/__main__.py
@@ -1,6 +1,6 @@
 import sys
 import time
-from typing import List
+from typing import List, Tuple
 import multiprocessing
 import itertools
 import functools
@@ -43,6 +43,14 @@ def make_requests(conf, progress, loop) -> List[stats.ForRequest]:
     return Benchmark(conf, progress).run()
 
 
+def requests_per_process(process_count: int, conf) -> Tuple[int, int]:
+    """Divides how many requests each forked process will make."""
+    return (
+        int(conf.concurrency / process_count),
+        int(conf.requests / process_count),
+    )
+
+
 def main(args: list=sys.argv[1:]) -> None:
     conf = cli.parse_args(args)
     if conf.use_uvloop:
@@ -51,6 +59,8 @@ def main(args: list=sys.argv[1:]) -> None:
     progress = stats.Progress()
 
     process_count = multiprocessing.cpu_count()
+    conf.concurrency, conf.requests = requests_per_process(process_count, conf)
+
     proc_pool = multiprocessing.Pool(processes=process_count)
     proc_stats, duration = utils.time_it(lambda: proc_pool.map(
         functools.partial(make_requests, conf, progress),
@@ -62,8 +72,8 @@ def main(args: list=sys.argv[1:]) -> None:
     progress.done()
 
     requests_stats = list(itertools.chain.from_iterable(proc_stats))
-    bench_results = stats.ForBenchmark(requests_stats, conf.concurrency,
-                                       duration).summary()
+    bench_results = stats.ForBenchmark(
+        requests_stats, conf.concurrency * process_count, duration).summary()
     print(stats.results_to_str(bench_results))
 
 
